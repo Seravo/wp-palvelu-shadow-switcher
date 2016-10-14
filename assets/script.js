1c9d2708f00cda759283ba349aed5e34d1631185
Christian Nikkanen <christian.nikkanen@gmail.com>
2016-10-14 11:32:19 +0300

    add event listeners for all links instead of the just one

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 (function(){
-  document.querySelector('#wp-admin-bar-wpis li > a').addEventListener('click', function(e){
-    e.preventDefault();
-    var instance = e.target.getAttribute('href').substr(1);
-    if (instance === 'exit') {
-      document.cookie = "wpp_shadow=;path=/";
-    } else {
-      document.cookie = "wpp_shadow=" + instance + ";path=/";
-    }
-    location.reload()
-  });
+  var links = document.querySelectorAll('#wp-admin-bar-wpis li > a')
+  for(var i = 0; i < links.length; i++){
+    var element = links[i]; 
+    element.addEventListener('click', function(e){
+      e.preventDefault();
+      var instance = e.target.getAttribute('href').substr(1);
+      if (instance === 'exit') {
+        document.cookie = "wpp_shadow=;path=/";
+      } else {
+        document.cookie = "wpp_shadow=" + instance + ";path=/";
+      }
+      location.reload()
+    });
+  }
 })();
