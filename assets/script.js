ead91bcc1ddf839ad4ff99d1a1ca96fd9ac59876
Christian Nikkanen <christian.nikkanen@gmail.com>
2016-10-14 11:33:56 +0300

    micro-optimization

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 (function(){
   var links = document.querySelectorAll('#wp-admin-bar-wpis li > a');
   for(var i = 0; i < links.length; i++){
-    var element = links[i]; 
-    element.addEventListener('click', function(e){
+    var element = links[i];
+    var listener = function(e){
       e.preventDefault();
       var instance = e.target.getAttribute('href').substr(1);
       if (instance === 'exit') {
@@ -11,6 +11,8 @@
         document.cookie = "wpp_shadow=" + instance + ";path=/";
       }
       location.reload();
-    });
+    };
+    
+    element.addEventListener('click', listener);
   }
 })();
