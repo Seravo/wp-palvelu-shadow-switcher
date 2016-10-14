ba573a04b41333d1a0a4eb98f9cb0d6de2f10540
Christian Nikkanen <christian.nikkanen@gmail.com>
2016-10-14 11:28:05 +0300

    rewrite to remove jQuery dependency

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-(function($) {
-$(document).on('click', '#wp-admin-bar-wpis li > a', function(e) {
-  e.preventDefault();
-  var instance = $(this).attr('href').substr(1);
-  if (instance == 'exit') {
-    document.cookie = "wpp_shadow=;path=/";
-  } else {
-    document.cookie = "wpp_shadow=" + instance + ";path=/";
-  }
-  location.reload();
-});
-})(jQuery);
+(function(){
+  document.querySelector('#wp-admin-bar-wpis li > a').addEventListener('click', function(e){
+    e.preventDefault();
+    var instance = e.target.getAttribute('href').substr(1);
+    if (instance === 'exit') {
+      document.cookie = "wpp_shadow=;path=/";
+    } else {
+      document.cookie = "wpp_shadow=" + instance + ";path=/";
+    }
+    location.reload()
+  });
+})();
